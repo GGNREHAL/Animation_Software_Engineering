c974c174c54ee8a289b92b81d5f2be972757ef9f
s5532166 <s5532166@bournemouth.ac.uk>
2023-01-22 15:06:17 +0000

    changes

--- tests.py
+++ tests.py
@@ -1,18 +1,33 @@
 import unittest
-
 from utils import get_transform
 from PIL import Image
 
+import numpy as np
+from MNIST_Fashion_Denoiser import Autoencoder()
 
-TEST_IMG_PATH = "test.jpg"
-
-class TestDataLoading(unittest.TestCase):
-    def test_transform(self):
-        img = Image.open(TEST_IMG_PATH)
-        transform = get_transform()
-        tensor  = transform(img)
-        self.assertEquals(tensor.shape[0], 3)
-        self.assertEquals(tensor.shape[1], 32)
-        self.assertEquals(tensor.shape[2], 32)
+class TestAutoencoder(unittest.TestCase):
+    def setUp(self):
+        self.model = Autoencoder()
         
+    def trainloader(self):
+        test_image = np.random.rand(28, 28)
+        output = self.model.predict(trainloader)
+        self.assertTrue(np.shape(output) == (28, 28))
+        
+    def testloader(self):
+        test_image = np.random.rand(28, 28)
+        test_label = np.random.randint(0, 10)
+        score = self.model.score(testloader)
+        self.assertTrue(score >= 0 and score <= 1)
+        
+    def val_loader(self):
+        test_image = np.random.rand(28, 28)
+        denoised_image = self.model.denoise(testloader)
+        self.assertTrue(np.shape(denoised_image) == (28, 28))
+        
+if __name__ == '__init__':
+    unittest.main()
+
+
+
         
--- utils.py
+++ utils.py
@@ -3,3 +3,11 @@ import torchvision.transforms as transforms
 
 def get_transform():
     return transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+
+def add_noise(data, noise_factor):
+    data += noise_factor * torch.randn(*data.shape)
+    data = torch.clamp(data, 0., 1.)
+    return data
+
+def denoising_loss(data, denoised_data):
+    return torch.nn.MSELoss()(data, denoised_data)
